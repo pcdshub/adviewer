65b2d7b38773127e0086b3be46a3437d10518e78
K Lauer <kenneth.lauer@gmail.com>
2019-03-25 15:57:26 -0700

    DOC: fix cookiecutter remnants

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0,module_path)
 # -- Project information -----------------------------------------------------
 
 project = 'adviewer'
-copyright = ('2018, {}'.format(SLAC National Accelerator Laboratory))
+copyright = '2018, SLAC National Accelerator Laboratory'
 author = 'SLAC National Accelerator Laboratory'
 
 # The short X.Y version
@@ -46,6 +46,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -111,7 +112,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'cookiecutterproject_namedoc'
+htmlhelp_basename = 'adviewer'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -138,8 +139,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'cookiecutterproject_name.tex', '\\{\\{ cookiecutter.project\\_name \\}\\} Documentation',
-     '\\{\\{ cookiecutter.autor\\_name \\}\\}', 'manual'),
+    (master_doc, 'adviewer.tex', 'adviewer Documentation',
+     'SLAC National Accelerator Laboratory', 'manual'),
 ]
 
 
@@ -148,7 +149,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'cookiecutterproject_name', 'adviewer Documentation',
+    (master_doc, 'adviewer', 'adviewer Documentation',
      [author], 1)
 ]
 
@@ -159,8 +160,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'cookiecutterproject_name', 'adviewer Documentation',
-     author, 'cookiecutterproject_name', 'One line description of project.',
+    (master_doc, 'adviewer', 'adviewer Documentation',
+     author, 'adviewer', 'AreaDetector viewer and configurator',
      'Miscellaneous'),
 ]
 
